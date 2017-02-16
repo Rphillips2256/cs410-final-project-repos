d671d47090c5e9e7f0a1f50460f4775ebe8f4834
Ryan <rphillips2256@gmail.com>
2017-02-16 14:54:02 -0600

    this should work

---
+++ Desktop/game/html/src/com/happyfuntime/com/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.happyfuntime.com.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.happyfuntime.com.GameLife;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new GameLife();
+        }
+}
